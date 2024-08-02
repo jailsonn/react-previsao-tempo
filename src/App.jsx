84f82be793915d0dd1173e5c75825c69f7c2ac21
jailsonn <jailson.nogueira2@gmail.com>
2024-08-02 20:04:07 -0300

    Iniciando com o CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
 import { useState, useRef } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./App.css";
-import Weatherinformation from './components/Weatherinformation/Weatherinformation'
+import WeatherInformations from "./components/WeatherInformations/WeatherInformations";
 
 function App() {
   // para colocar na tela usa-se um stado (useState)
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState();
   // O useRef é para referir ou pegar o valor digitado no input
-  const inputRef = useRef()
-
+  const inputRef = useRef();
 
   // função principal (promiss)
   async function searchCity() {
-    const city = inputRef.current.value
+    const city = inputRef.current.value;
     const key = "cebcd482eda57fa9a6714c1c2ba91885";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric&lang=pt_br`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric&lang=pt_br`;
 
     // console.log(inputRef.current.value);
 
-    const apiData = await axios.get(url)
-    setWeather(apiData.data)
+    const apiData = await axios.get(url);
+    // só quero o data
+    setWeather(apiData.data);
     // console.log(apiData)
-    
+
   }
 
   return (
-    <div>
+    <div className="container">
       <h1>Previsão do Tempo</h1>
       <input ref={inputRef} type="text" placeholder="Digite o nome da Cidade" />
       <button onClick={searchCity}>Buscar</button>
 
-      <Weatherinformation weather={weather} />
+      {weather && <WeatherInformations weather={weather}/>}
     </div>
   );
 }
--- src/components/WeatherInformations/WeatherInformations.jsx
+++ src/components/WeatherInformations/WeatherInformations.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 
-function Weatherinformation({ weather }) {
+function WeatherInformations({ weather }) {
   console.log(weather);
 
   return (
     <div>
       <h2>{weather.name}</h2>
+
       <div>
         <img
           src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
@@ -14,7 +15,7 @@ function Weatherinformation({ weather }) {
       </div>
       <p>{weather.weather[0].description}</p>
       <div>
-        <p>Sensação térmica: {Math.round(weather.main.feels_like)}</p>
+        <p>Sensação térmica: {Math.round(weather.main.feels_like)}ºC</p>
         <p>Umidade: {weather.main.humidity} %</p>
         <p>Pressão: {Math.round(weather.main.pressure)}</p>
       </div>
@@ -22,4 +23,4 @@ function Weatherinformation({ weather }) {
   );
 }
 
-export default Weatherinformation;
+export default WeatherInformations;
