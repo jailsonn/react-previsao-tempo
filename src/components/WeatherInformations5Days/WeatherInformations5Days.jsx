66818bd13d56844fceee8a18d145b00df3a027b6
jailsonn <jailson.nogueira2@gmail.com>
2024-08-04 01:03:41 -0300

    modificando os componentes, e finalizando

--- src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
+++ src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
@@ -7,14 +7,50 @@ function WeatherInformations5Days({ weather5Days }) {
   let dailyForecast = {};
 
   for (let forecast of weather5Days.list) {
-    const date = new Date(forecast.dt * 1000).toLocaleDateString()
+    const date = new Date(forecast.dt * 1000).toLocaleDateString();
 
-    console.log(date);
+    // console.log(date);
+
+    if (!dailyForecast[date]) {
+      dailyForecast[date] = forecast;
+    }
+  }
+  //   console.log(dailyForecast);
+  //   const nextFiveDays = Object.values(dailyForecast).slice(1.6)
+  //   console.log(nextFiveDays);
+
+  const next5DaysForecast = Object.values(dailyForecast).slice(1.6);
+  //   console.log(next5DaysForecast)
+
+  function convertDate(date) {
+    const newDate = new Date(date.dt * 1000).toLocaleDateString("pt-BR", {
+      weekday: "long",
+      day: "2-digit",
+    });
+
+    // console.log(newDate);
+    return newDate;
   }
 
   return (
-    <div>
-      <p>5 days</p>
+    <div className="weather-container">
+      <h3>Previsão próximos 5 Dias</h3>
+      <div className="weather-list">
+        {next5DaysForecast.map((forecast) => (
+          <div key={forecast.dt} className="weather-item">
+            <p className="forecast-day">{convertDate(forecast)}</p>
+            <img
+              src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
+              alt="imagem-tempo"
+            />
+            <p className="forecast-description">{forecast.weather[0].description}</p>
+            <p>
+              {Math.round(forecast.main.temp_min)}ºC min /{" "}
+              {Math.round(forecast.main.temp_max)}ºC máx
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
